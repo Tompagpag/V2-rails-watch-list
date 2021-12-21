848c79996163050cedd2178d8604708516539c9d
Tom Paganel <paganel.tom@gmail.com>
2021-12-21 17:01:14 +0100

    add link in navbar

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,19 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
-  get 'lists', to: 'lists#index'
-  get 'lists/:id', to: 'lists#show'
+  resources :lists, only: [:index, :new, :create, :show]
 end
